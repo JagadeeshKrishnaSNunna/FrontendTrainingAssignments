460b7619cb1f56d399c443da0ef5af02803fc828
jagadeesh <jk.nunna1602@gmail.com>
2025-10-02 11:25:36 -0400

    serch by name

---
+++ SearchByName/search.js
@@ -0,0 +1,118 @@
+var USERS;
+function parseNumber(num) {
+    num = num.split(" ")[0];
+    num = num.replaceAll('.', '-')
+    num = num.replace('(', '')
+    num = num.replace(')', '-')
+    if (num[0] == 1) {
+        num = '+' + num
+    }
+    else {
+        num = '+1-' + num
+    }
+    return num;
+}
+
+function display(){
+    document.getElementById('arrow').classList.toggle('turn')
+    document.getElementById('details').classList.toggle('detailsDisplay')
+}
+
+function parseMore(data){
+    data=data.substring(1,data.length-1)
+    data=data.replaceAll(',','<br>')
+    data=data.replaceAll('"','')
+    data=data.replaceAll(':',' : ')
+    data=data.replaceAll('{','<br>{<br>')
+    data=data.replaceAll('}','<br>}<br>')
+    console.log(data)
+    return data
+}
+async function Users() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const user = await res.json();
+    return user;
+}
+
+function find() {
+    var input = document.getElementById("input");
+    var name = input.value;
+    input.value=""
+    var match = []
+    document.getElementById("card_dock").innerHTML = ''
+    for (let i = 0; i < USERS.length; i++) {
+        if (USERS[i].name.toLowerCase().includes(name.toLowerCase())) {
+            match.push((USERS[i]))
+        }
+    }
+    if (match.length) {
+        match.forEach(item => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+
+            card.innerHTML = `
+        <div class="name">
+                <h2>${item.name}</h3>
+        </div>
+        <div class="userLogo">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
+            <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5"/>
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+        </svg>
+        </div>
+        <div class="email">
+            <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-envelope-at-fill" viewBox="0 0 16 16">
+                    <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2zm-2 9.8V4.698l5.803 3.546zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.5 4.5 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586zM16 9.671V4.697l-5.803 3.546.338.208A4.5 4.5 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671"/>
+                    <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791"/>
+                </svg>
+                <span>${item.email}</span>
+            </p>
+        </div>
+        <div class="phone">
+            <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                </svg>
+                <span>${parseNumber(item.phone)}</span>
+            </p>
+        </div>
+        <div class="address">
+            <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+                  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
+                </svg>
+                <span>${item.address.street},</span><br>
+                <span style="margin-left:40px">${item.address.suite},</span><br>
+                <span style="margin-left:40px">${item.address.city},</span><br>
+                <span style="margin-left:40px">${item.address.zipcode}.</span>
+            </p>
+        </div>
+        <div class="more">
+            <div class="moreEx" onclick="display()">
+                <h6>MORE >></h6>
+                <div id="arrow">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-caret-up-fill" viewBox="0 0 16 16">
+                  <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+                </svg>
+                </div>
+            </div>
+            <div id="details" class="di">
+                <hr>
+                <p>${parseMore(JSON.stringify(item))}</p>
+            </div>    
+        </div>
+        `;
+
+            document.getElementById("card_dock").appendChild(card);
+        });
+    }
+
+}
+
+(async () => {
+    USERS = await Users()
+    butt = document.getElementById("searchButton")
+    butt.addEventListener('click', find)
+})()
+
