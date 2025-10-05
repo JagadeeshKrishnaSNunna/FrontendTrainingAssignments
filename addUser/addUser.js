02ec5120478ad7d86aaf9925daec5c698b4daf9b
jagadeesh <jk.nunna1602@gmail.com>
2025-10-05 08:46:45 -0400

    del user action updated

--- addUser/addUser.js
+++ addUser/addUser.js
@@ -1,16 +1,25 @@
 var data=new Array();
-
+function del(ele){
+    var dataToDelete=ele.parentNode.previousElementSibling.innerHTML;
+    var tr=ele.parentNode.parentNode;
+    data=data.filter(d => d!==dataToDelete)
+    tr.style.display='none' 
+    console.log(data);
+       
+}
 function add(){
-    // console.log("ol")
 var inp=document.getElementById("input").value
 document.getElementById("input").value=""
 if(data.includes(inp)){
-    window.alert("User already exist")
+    window.alert(inp+" already exist")
 }else{
     data.push(inp)
     var newRow=document.createElement('tr')
     newRow.innerHTML=`
-    <tr><td>${inp}</td></tr>
+    <tr>
+        <td>${inp}</td>
+        <td><button onclick="del(this)">Delete</button></td>
+    </tr>
     `
     document.getElementById("list").appendChild(newRow)
 }
